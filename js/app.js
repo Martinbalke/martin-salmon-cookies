a3b6dabe7148db9199e2b6b4aaf6ca17f0161cb0
Martin Balke <Mrtnbalke@gmail.com>
2019-05-22 19:07:11 -0700

    turned my objects into constructor functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,4 @@
-//every store needs to be it's own object with the values of min and max hourly customers, and average cookies per customer in object properties
-
-// each store needs a method that generates a random number of customers per hour based on the min max
-
-//use the object method to create a number of cookies sold per hour and store that data in an object.
-
-// results of cookies per hour need to be displayed on the page in an unordered list with a total calculated sales
-
+'use strict';
 //modeling out the elements I will need in the DOM
 const ul_salesFirstAndPike = document.getElementById('salesFirstAndPike');
 const ul_salesSeatacAirport = document.getElementById('salesSeatacAirport');
@@ -15,11 +8,11 @@ const ul_salesAlki = document.getElementById('salesAlki');
 
 //takes in a stores min and max customers and generates a random number of customers per hour
 let customerNumber = function(store){
-  return Math.floor(Math.random() * (store.maxCust - store.minCust) + store.minCust);
+  return Math.ceil(Math.random() * (store.maxCust - store.minCust + 1) + store.minCust);
 };
 //generates a random number of cookies sold at each store
 let cookiesSold = function(store){
-  return Math.floor(customerNumber(store) * store.avgCookies);
+  return Math.ceil(customerNumber(store) * store.avgCookies);
 };
 //logs the cookies sold each hour and pushes them on to the store as an object
 
@@ -42,59 +35,20 @@ let cookiesPerDay = function(store){
   }
   store.cookiesSold[`Total: `] = total;
 };
-
-let firstAndPike ={
-  minCust: 23,
-  maxCust: 65,
-  avgCookies: 6.3,
-  cookiesSold: {
-
-  }
-};
-
-let seaTacAirport ={
-  minCust: 3,
-  maxCust: 24,
-  avgCookies: 1.2,
-  cookiesSold: {
-
-  }
-};
-
-let seattleCenter ={
-  minCust: 11,
-  maxCust: 38,
-  avgCookies: 2.3,
-  cookiesSold: {
-
-  }
-};
-
-let capitolHill = {
-  minCust: 20,
-  maxCust: 38,
-  avgCookies: 2.3,
-  cookiesSold: {
-
-  }
-};
-
-let alki ={
-  minCust: 2,
-  maxCust: 16,
-  avgCookies: 4.6,
-  cookiesSold: {
-
-  }
-};
+function StoreConstructor(minCust, maxCust, avgCookies){
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookies = avgCookies;
+  this.cookiesSold = {};
+}
+const firstAndPike = new StoreConstructor(23, 65, 6.3);
+const seaTacAirport = new StoreConstructor(3, 24, 1.2);
+const seattleCenter = new StoreConstructor(11, 38, 2.3);
+const capitolHill = new StoreConstructor(20, 38, 2.3);
+const alki = new StoreConstructor(2, 16, 4.6);
 
 //calling the function and populating the cookies sold objects
-cookiesPerDay(firstAndPike);
-cookiesPerDay(seattleCenter);
-cookiesPerDay(seaTacAirport);
-cookiesPerDay(capitolHill);
-cookiesPerDay(alki);
-
+[firstAndPike, seattleCenter, seaTacAirport, capitolHill, alki].forEach(cookiesPerDay);
 
 //iterates over the nested objects inside each store and displays them inside of a li which is appended to the parent UL
 const iterateAndDisplay = function(obj, location){
@@ -102,8 +56,6 @@ const iterateAndDisplay = function(obj, location){
     let docLI = document.createElement('li');
     docLI.appendChild(document.createTextNode(`${key}${obj[key]}`));
     location.appendChild(docLI);
- 
-
   });
 };
 
@@ -113,5 +65,3 @@ iterateAndDisplay(seattleCenter.cookiesSold, ul_salesSeattleCenter);
 iterateAndDisplay(seaTacAirport.cookiesSold, ul_salesSeatacAirport);
 iterateAndDisplay(capitolHill.cookiesSold, ul_salesCapitolHill);
 iterateAndDisplay(alki.cookiesSold, ul_salesAlki);
-
-
